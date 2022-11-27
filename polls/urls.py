b8ad66684646f275944fc2f3f57b9da2564d0ed5
SienTien <hsientien0212@gmail.com>
2022-11-27 17:57:55 -0500

    change directory

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('ISP/', views.index, name='index'),
+    path('', views.index, name='index'),
   
 ]
--- polls/views.py
+++ polls/views.py
@@ -2,12 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def calc():
-    x = 1
-    y = 2
-    return x+y
-
 def index(request):
-    x = calc()
-    y = 2
-    return render(request,  'hello.html', {'name': ''})
+    return render(request,  'hello.html')
